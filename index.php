da3f4f1aa2c242fa2fd58319c70e60e4e95f0ace
Emanuel-Soares <emanuelsevero869@gmail.com>
2021-01-08 16:18:42 -0300

    estrutura inicial de arquivos

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-Página inicial
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head><?php include '/common/headContent.php' ?></head>
+<body>
+  <?php include '/common/header.php' ?>
+  <?php include '/common/footer.php' ?>
+</body>
+</html>
