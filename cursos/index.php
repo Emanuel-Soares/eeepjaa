c67896240b84f9b214fc08c1a4510c47c53cda24
JoseVitorNobre <josevitortinf@gmail.com>
2021-01-08 21:24:49 -0300

    linkagens corrigidas

--- cursos/index.php
+++ cursos/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head><?php include '/common/headContent.php' ?></head>
+<head><?php include 'http://beta.eeepjaa.site/common/headContent.php' ?></head>
 <body>
   <?php include '/common/header.php' ?>
   <main></main>
--- sobre/index.php
+++ sobre/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head><?php include '/common/headContent.php' ?></head>
+<head><?php include 'http://beta.eeepjaa.site/common/headContent.php' ?></head>
 <body>
-  <?php include '/common/header.php' ?>
+  <?php include 'http://beta.eeepjaa.site/common/header.php' ?>
   <main></main>
-  <?php include '/common/footer.php' ?>
+  <?php include 'http://beta.eeepjaa.site/common/footer.php' ?>
 </body>
 </html>
