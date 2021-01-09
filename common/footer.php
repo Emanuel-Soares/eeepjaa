b33a0d4d66ed0716f8c8dbf9fa3c818556d510b2
Emanuel Soares <46508229+Emanuel-Soares@users.noreply.github.com>
2021-01-09 13:45:00 -0300

    responsividade do footer melhorada

--- common/footer.php
+++ common/footer.php
@@ -1,6 +1,6 @@
 <footer class="page-footer">
    <!-- Desktop -->
-   <div class="row hide-on-small-only">
+   <div class="row hide-on-med-and-down">
       <div class="col l6 s4">
          <h5 class="white-text bold">EEEP Joaquim Antônio Albano</h5>
          <p class="grey-text text-lighten-4">Rua Júlio Siqueira,390 - Dionísio Torres</p>
@@ -20,7 +20,7 @@
    </div>
    <!-- Mobile -->
 
-      <div class="row hide-on-med-and-up">
+      <div class="row hide-on-large-only">
          <div class="col l6 s6">
             <h5 class="white-text bold">EEEP Joaquim Antônio Albano</h5>
             <p class="grey-text text-lighten-4">Rua Júlio Siqueira, 390 - Dionísio Torres</p>
@@ -33,11 +33,12 @@
                <p class="white-text">Sidio Loiola - Davidson Alves</p>
          </div>
       </div>
-      <div class="row hide-on-med-and-up">
-      <div class="col s12">
+      <div class="row hide-on-large-only">
+        <div class="col s12">
             <iframe style="position: relative; width: 100%; height: 200px; margin-top:-20px" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15925.115504220736!2d-38.5099877!3d-3.7493274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7eb2e63b55439b21!2sEEEP%20Professor%20Joaquim%20Ant%C3%B4nio%20Albano!5e0!3m2!1spt-BR!2sbr!4v1610156891468!5m2!1spt-BR!2sbr" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
          </div>
       </div>
+    </div>
    <div class="footer-copyright">
      <div class="container" style="display: flex;justify-content: space-between;align-items: center;">
      <span class="grey-text text-lighten-4 right left">© EEEP Joaquim Antônio Albano, 2021</span>
