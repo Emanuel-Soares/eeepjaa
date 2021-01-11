eb193863e7ff72010c20dca378849f9b8b158868
JoseVitorNobre <josevitortinf@gmail.com>
2021-01-11 15:41:14 -0300

    Slider incompleto com imagens

--- common/headContent.php
+++ common/headContent.php
@@ -4,12 +4,13 @@
 <meta http-equiv="X-UA-Compatible" content="IE=7" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
-<!-- CND -->
+<!-- CDN -->
 <link href="https:/fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <script src="https:/code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https:/cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 <script src="https:/cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Atma:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <!-- CSS Local -->
 <link rel="stylesheet" href="/assets/css/index.css">
 <link rel="stylesheet" href="/assets/css/header.css">
