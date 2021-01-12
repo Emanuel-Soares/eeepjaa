093ea7a8d17d0ebfd5c305f7970b85fef437e1c7
Emanuel-Soares <emanuelsevero869@gmail.com>
2021-01-12 18:14:01 -0300

    altura do google maps no desktop alterada para 200

--- common/footer.php
+++ common/footer.php
@@ -1,47 +1,47 @@
 <footer class="page-footer teal darken-4">
-   <!-- Desktop -->
-   <div class="row hide-on-med-and-down">
-      <div class="col s4">
-         <h5 class="white-text bold">EEEP Joaquim Antônio Albano</h5>
-         <p class="grey-text text-lighten-4">R. Júlio Siqueira, 390 - Dionísio Torres, Fortaleza - CE, 60120-320</p>
-         <a href="https:/www.facebook.com/eeepjalbano" target="_blank"><img src="/assets/icons/Facebook.svg" height="40px" style="filter:invert(100%)" title="Facebook"></a>
-         <a href="https:/www.instagram.com/eeepjoaquimalbano/" target="_blank"><img src="/assets/icons/Instagram.svg" height="40px" style="filter:invert(100%); padding: 0 0 0 5px" title="Instagram"></a>
-      </div>
-      <div class="col s4 center">
-         <h5 class="white-text">Orientadores</h5>
-         <p class="white-text">Carlos Henrique</p>
-         <p class="white-text">Sidio Loiola - Davidson Alves</p>
-      </div>
-      <div class="col s4">
-         <iframe style="position: absolute; right: 0; width: 30%; height: 180px; margin-top:-20px;border: 0;" src="https:/www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15925.115504220736!2d-38.5099877!3d-3.7493274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7eb2e63b55439b21!2sEEEP%20Professor%20Joaquim%20Ant%C3%B4nio%20Albano!5e0!3m2!1spt-BR!2sbr!4v1610156891468!5m2!1spt-BR!2sbr " frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-      </div>
-   </div>
-   <!-- Mobile -->
+  <!-- Desktop -->
+  <div class="row hide-on-med-and-down">
+    <div class="col s4">
+      <h5 class="white-text bold">EEEP Joaquim Antônio Albano</h5>
+      <p class="grey-text text-lighten-4">R. Júlio Siqueira, 390 - Dionísio Torres, Fortaleza - CE, 60120-320</p>
+      <a href="https:/www.facebook.com/eeepjalbano" target="_blank"><img src="/assets/icons/Facebook.svg" height="40px" style="filter:invert(100%)" title="Facebook"></a>
+      <a href="https:/www.instagram.com/eeepjoaquimalbano/" target="_blank"><img src="/assets/icons/Instagram.svg" height="40px" style="filter:invert(100%); padding: 0 0 0 5px" title="Instagram"></a>
+    </div>
+    <div class="col s4 center">
+      <h5 class="white-text">Orientadores</h5>
+      <p class="white-text">Carlos Henrique</p>
+      <p class="white-text">Sidio Loiola - Davidson Alves</p>
+    </div>
+    <div class="col s4">
+      <iframe style="position: absolute; right: 0; width: 30%; height: 200px; margin-top:-20px;border: 0;" src="https:/www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15925.115504220736!2d-38.5099877!3d-3.7493274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7eb2e63b55439b21!2sEEEP%20Professor%20Joaquim%20Ant%C3%B4nio%20Albano!5e0!3m2!1spt-BR!2sbr!4v1610156891468!5m2!1spt-BR!2sbr " frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+  </div>
+  <!-- Mobile -->
 
-   <div class="row hide-on-large-only">
-      <div class="col l6 s6">
-         <h5 class="white-text bold">EEEP Joaquim Antônio Albano</h5>
-         <p class="grey-text text-lighten-4">R. Júlio Siqueira, 390 - Dionísio Torres, Fortaleza - CE, 60120-320</p>
-         <a href="https:/www.facebook.com/eeepjalbano" target="_blank"><img src="/assets/icons/Facebook.svg" height="40px" style="filter:invert(100%)" title="Facebook"></a>
-         <a href="https:/www.instagram.com/eeepjoaquimalbano/" target="_blank"><img src="/assets/icons/Instagram.svg" height="40px" style="filter:invert(100%); padding: 0 0 0 5px" title="Instagram"></a>
-      </div>
-      <div class="col l6 s6 center">
-         <h5 class="white-text">Orientadores</h5>
-         <p class="white-text">Carlos Henrique</p>
-         <p class="white-text">Sidio Loiola - Davidson Alves</p>
-      </div>
-   </div>
-   <div class="row hide-on-large-only">
-      <div class="col s12">
-         <iframe style="position: relative; width: 100%; height: 200px; margin-top:-20px" src="https:/www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15925.115504220736!2d-38.5099877!3d-3.7493274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7eb2e63b55439b21!2sEEEP%20Professor%20Joaquim%20Ant%C3%B4nio%20Albano!5e0!3m2!1spt-BR!2sbr!4v1610156891468!5m2!1spt-BR!2sbr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-      </div>
-   </div>
-   </div>
-   <div class="footer-copyright">
-      <div class="container" style="display: flex;justify-content: space-between;align-items: center;">
-         <span class="grey-text text-lighten-4 right left">© EEEP Joaquim Antônio Albano, 2021</span>
-         <a class="grey-text text-lighten-4 right" href="mailto:primeiroinformaticaeeep40@gmail.com">Feito pelo 3° Informática 2020</a>
-      </div>
-   </div>
+  <div class="row hide-on-large-only">
+    <div class="col l6 s6">
+      <h5 class="white-text bold">EEEP Joaquim Antônio Albano</h5>
+      <p class="grey-text text-lighten-4">R. Júlio Siqueira, 390 - Dionísio Torres, Fortaleza - CE, 60120-320</p>
+      <a href="https:/www.facebook.com/eeepjalbano" target="_blank"><img src="/assets/icons/Facebook.svg" height="40px" style="filter:invert(100%)" title="Facebook"></a>
+      <a href="https:/www.instagram.com/eeepjoaquimalbano/" target="_blank"><img src="/assets/icons/Instagram.svg" height="40px" style="filter:invert(100%); padding: 0 0 0 5px" title="Instagram"></a>
+    </div>
+    <div class="col l6 s6 center">
+      <h5 class="white-text">Orientadores</h5>
+      <p class="white-text">Carlos Henrique</p>
+      <p class="white-text">Sidio Loiola - Davidson Alves</p>
+    </div>
+  </div>
+  <div class="row hide-on-large-only">
+    <div class="col s12">
+      <iframe style="position: relative; width: 100%; height: 200px; margin-top:-20px" src="https:/www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15925.115504220736!2d-38.5099877!3d-3.7493274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7eb2e63b55439b21!2sEEEP%20Professor%20Joaquim%20Ant%C3%B4nio%20Albano!5e0!3m2!1spt-BR!2sbr!4v1610156891468!5m2!1spt-BR!2sbr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+  </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container" style="display: flex;justify-content: space-between;align-items: center;">
+      <span class="grey-text text-lighten-4 right left">© EEEP Joaquim Antônio Albano, 2021</span>
+      <a class="grey-text text-lighten-4 right" href="mailto:primeiroinformaticaeeep40@gmail.com">Feito pelo 3° Informática 2020</a>
+    </div>
+  </div>
 </footer>
 <script src="/assets/js/index.js"></script>
