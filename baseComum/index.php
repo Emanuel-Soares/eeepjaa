dfc6ea02bb853570d8920abfa7965ee1198b6b6d
Cupnodes <aldairgomesdecastrosales@gmail.com>
2021-01-13 03:06:58 -0300

    Added the first version of the baseComum page.

---
+++ baseComum/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <?php include '../common/headContent.php' ?>
+   <title>BNCC - Informações Diversas</title>
+</head>
+<body>
+    <?php include '../common/header.php' ?>
+        <div class="container">
+            <section>
+                <h3 class=" grey-text text-darken-4">
+                    O que é Ensino Técnico Integrado?
+                </h3>
+
+                <p class="grey-text text-lighten-1 flow-text">
+                    Nessa modalidade de ensino, o estudante cursa o Ensino Médio junto com um
+                    curso técnico, já terminando seu ensino com uma profissão. No fim do curso,
+                    o aluno recebe dois certificados, um de conclusão do 
+                    médio e outro de conclusão do técnico.
+                </p>
+            </section>
+
+            <section>
+                <h3 class=" grey-text text-darken-4">
+                    O que é  Ensino de Tempo Integral?
+                </h3>
+
+                <p class="grey-text text-lighten-1 flow-text">
+                    O aluno que ingressar em uma escola de tempo integral teráa oportunidade
+                    de acompanhar o ensino básico das demais instituições enquanto aproveitar
+                    aulas técnicas parabuscar se tornar um profissional diferenciado
+                    no mercado de trabalho.
+                </p>
+            </section>
+            <section>
+                <h3 class=" grey-text text-darken-4">
+                    Como funcionam as aulas no JA?
+                </h3>
+
+                <p class="grey-text text-lighten-1 flow-text">
+                    O JA segue as diretrizes da <span class="amber-text text-accent-4">BNCC (Base Nacional Curricular Comum)</span>, trazendo
+                    na grade curricular tanto uma formação geral, voltada para os vestibulares
+                    tradicionais quanto uma base diversificada: visando a maior 
+                    capacitação técnica, social e cognitiva dos nossos alunos.
+                </p>
+
+                <h4 class="green-text text-accent-4">
+                    1. Formação Geral
+                </h4>
+
+                <p class="flow-text grey-text text-ligthen-1">
+                    O papel da nossa escola é garantir a formação intelectual do seu filho, para tanto
+                    nós oferecemos o conhecimento geral que o estudante  vai precisar para
+                    ingressar nas principais universidades em nível estadual e nacional.
+                </p>
+
+                <h4 class="green-text text-accent-4">
+                    2. Base Diversificada
+                </h4>
+
+                <p class="flow-text grey-text text-ligthen-1">
+                    Além das aulas voltadas para o conhecimento geral do ensino médio, nossa 
+                    instituição de ensino oferece a oportunidade de reflexão por parte do aluno
+                    quanto a sua carreira, temos aulas de empreendedorismo, essenciais para o
+                    florescimento do espírito inventido dos nossos jovens, bem como práticas de 
+                    debate sobre a realidade profissional que os aguarda: Projeto de Vida, Formação
+                    Cidadã e Mundo do Trabalho.
+                </p>
+            </section>
+        </div>
+    <?php include '../common/footer.php' ?>
+</body>
+</html>
