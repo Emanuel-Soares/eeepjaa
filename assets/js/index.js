c7b32ab0f5d91b201e10674c904d7a2c5911ccb8
Emanuel-Soares <emanuelsevero869@gmail.com>
2021-01-18 11:52:26 -0300

    slider refatorado (incompleto)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,25 @@
 $(document).ready(() => {
-  $(".button-collapse").sideNav()
-  $(".carousel").carousel({ fullWidth: true, duration: 100 })
-  $(".slider").slider()
-  $(".collapsible").collapsible()
+  $(".button-collapse").sideNav();
+  $(".carousel").carousel({ fullWidth: true, duration: 100 });
+  $(".slider").slider();
+  $(".collapsible").collapsible();
 
-  $("#carousel-prev-button").click(() => $(".carousel").carousel("prev"))
-  $("#carousel-next-button").click(() => $(".carousel").carousel("next"))
- 
-  $('.modal').modal()
+  $("#carousel-prev-button").click(() => $(".carousel").carousel("prev"));
+  $("#carousel-next-button").click(() => $(".carousel").carousel("next"));
+
+  $(".modal").modal();
 
   setInterval(() => {
-    $('.carousel').carousel('next')
-  }, 5000)
-})
+    $(".carousel").carousel("next");
+  }, 8000);
+
+  $.fn.preload = function () {
+    this.each(function () {
+      $("<img/>").src = this;
+    });
+  };
+
+  $("img").each((i, e) => {
+    $(e).preload();
+  });
+});
--- components/home/slider.php
+++ components/home/slider.php
@@ -1,9 +1,9 @@
 <div class="row" style="margin: 0 auto">
-  <div class="col l5 s12" id="image-fixed">
+  <div class="col l5 hide-on-med-and-down" id="image-fixed">
     <img src="/assets/img/home/eeep.png" alt="EEEPJAA">
   </div>
   <div class="col l7 s12" style="padding: 0">
-    <div class="carousel carousel-slider center slider" data-indicators="true">
+    <div class="carousel carousel-slider center slider" data-indicators="true" id="slider">
       <div class="carousel-fixed-item space-between">
         <button id="carousel-prev-button" class="btn-floating waves-effect btn-medium">
           <i class="material-icons">arrow_back</i>
@@ -15,7 +15,7 @@
       <div class="carousel-item white-text">
         <img src="/assets/img/slider/sisu_2019.png" alt="Sisu 2019" class="slider-image">
       </div>
-      <div class="carousel-item white-text">
+      <div class="carousel-item white-text" id="last-slide">
         <img src="/assets/img/home/enfermagem.png" alt="Sisu 2019" class="slider-image">
       </div>
     </div>
