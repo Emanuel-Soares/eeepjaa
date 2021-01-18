956caf0032c5e32e349ca605ca471cb3db56a1fc
Emanuel-Soares <emanuelsevero869@gmail.com>
2021-01-18 11:49:07 -0300

    barra verde sem conteúdo

--- common/header.php
+++ common/header.php
@@ -1,84 +1,84 @@
 <header>
-    <div class="teal darken-4 white-text center">
-        <a class="white-text" href="tel:08531012072">
+  <div class="teal darken-4 white-text center" style="height: 10px">
+    <!-- <a class="white-text" href="tel:08531012072">
             <sub><i class="tiny material-icons white-text">phone</i></sub>
             (85) 3101-2072</a> |
-        <a class="white-text" href="mailto:eeepjalbano@gmail.com"><sub><i class="tiny material-icons" style="position: relative;top: 50%;">email</i></sub> eeepjalbano@gmail.com</a>
+        <a class="white-text" href="mailto:eeepjalbano@gmail.com"><sub><i class="tiny material-icons" style="position: relative;top: 50%;">email</i></sub> eeepjalbano@gmail.com</a> -->
+  </div>
+  <nav class="grey lighten-4">
+    <div class="nav-wrapper">
+      <a href="/" id="logo" class="brand-logo orange-text text-darken-3">
+        <img class="logo" src="/assets/icons/EEEPJAA.png" alt="EEEP JAA" title="EEEP JAA">
+      </a>
+      <a style="cursor: pointer;" data-activates="mobile-demo" class="button-collapse orange-text"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li>
+          <a class="orange-text" href="/">
+            Home
+          </a>
+        </li>
+        <li>
+          <a class="orange-text" href="/cursos">
+            Cursos
+          </a>
+        </li>
+        <li>
+          <a class="orange-text" href="/baseComum">
+            Base Comum
+          </a>
+        </li>
+        <li>
+          <a class="orange-text" href="/gestao">
+            Gestão
+          </a>
+        </li>
+        <li>
+          <a class="orange-text" href="/eventos">
+            Eventos
+          </a>
+        </li>
+        <li>
+          <a class="orange-text" href="/sobre">
+            Sobre
+          </a>
+        </li>
+      </ul>
     </div>
-    <nav class="grey lighten-4">
-        <div class="nav-wrapper">
-            <a href="/" id="logo" class="brand-logo orange-text text-darken-3">
-                <img class="logo" src="/assets/icons/EEEPJAA.png" alt="EEEP JAA" title="EEEP JAA">
-            </a>
-            <a style="cursor: pointer;" data-activates="mobile-demo" class="button-collapse orange-text"><i class="material-icons">menu</i></a>
-            <ul class="right hide-on-med-and-down">
-                <li>
-                    <a class="orange-text" href="/">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a class="orange-text" href="/cursos">
-                        Cursos
-                    </a>
-                </li>
-                <li>
-                    <a class="orange-text" href="/baseComum">
-                        Base Comum
-                    </a>
-                </li>
-                <li>
-                    <a class="orange-text" href="/gestao">
-                        Gestão
-                    </a>
-                </li>
-                <li>
-                    <a class="orange-text" href="/eventos">
-                        Eventos
-                    </a>
-                </li>
-                <li>
-                    <a class="orange-text" href="/sobre">
-                        Sobre
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+  </nav>
 
-    <ul class="side-nav grey lighten-4" id="mobile-demo">
-        <div style="margin: 20px 0 0 80px;">
-            <a href="/" id="logo" class="brand-logo orange-text text-darken-3"><img src="/assets/icons/EEEPJAA.png" height="90px" style="margin-top: -8px; margin-left: 10px" alt="EEEP JAA" title="EEEP JAA"></a>
-        </div>
-        <li>
-            <a class="orange-text text-darken-4 center-align" href="/">
-                Home
-            </a>
-        </li>
-        <li>
-            <a class="orange-text text-darken-4 center-align" href="/cursos">
-                Cursos
-            </a>
-        </li>
-        <li>
-            <a class="orange-text text-darken-4 center-align" href="/baseComum">
-                Base
-            </a>
-        </li>
-        <li>
-            <a class="orange-text text-darken-4 center-align" href="/gestao">
-                Gestão
-            </a>
-        </li>
-        <li>
-            <a class="orange-text text-darken-4 center-align" href="/eventos">
-                Eventos
-            </a>
-        </li>
-        <li>
-            <a class="orange-text text-darken-4 center-align" href="/sobre">
-                Sobre
-            </a>
-        </li>
-    </ul>
+  <ul class="side-nav grey lighten-4" id="mobile-demo">
+    <div style="margin: 20px 0 0 80px;">
+      <a href="/" id="logo" class="brand-logo orange-text text-darken-3"><img src="/assets/icons/EEEPJAA.png" height="90px" style="margin-top: -8px; margin-left: 10px" alt="EEEP JAA" title="EEEP JAA"></a>
+    </div>
+    <li>
+      <a class="orange-text text-darken-4 center-align" href="/">
+        Home
+      </a>
+    </li>
+    <li>
+      <a class="orange-text text-darken-4 center-align" href="/cursos">
+        Cursos
+      </a>
+    </li>
+    <li>
+      <a class="orange-text text-darken-4 center-align" href="/baseComum">
+        Base
+      </a>
+    </li>
+    <li>
+      <a class="orange-text text-darken-4 center-align" href="/gestao">
+        Gestão
+      </a>
+    </li>
+    <li>
+      <a class="orange-text text-darken-4 center-align" href="/eventos">
+        Eventos
+      </a>
+    </li>
+    <li>
+      <a class="orange-text text-darken-4 center-align" href="/sobre">
+        Sobre
+      </a>
+    </li>
+  </ul>
 </header>
