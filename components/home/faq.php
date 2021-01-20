6dd2fe3f6ef7498c58c70be40492112e4643b2bd
Diego-Melo <diego.nac@hotmail.com>
2021-01-19 22:27:12 -0300

    Finalizada a estrutura do 'Pergunta Frequentes'

--- components/home/faq.php
+++ components/home/faq.php
@@ -1,7 +1,29 @@
 <div class="row">
-  <div class="container">
+  <div class="container teal-text text-darken-4">
     <h2 class="atma faq-heading teal-text text-darken-4 center">
       Perguntas Frequentes
     </h2>
+
+    <ul class="collapsible popout" data-collapsible="accordion">
+      <li>
+        <div class="collapsible-header valign-wrapper faq-border">
+          <i class="material-icons orange-text dot-size ">fiber_manual_record</i>
+          <h5>
+            First
+          </h5>
+        </div>
+        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li>
+      <li>
+        <div class="collapsible-header valign-wrapper faq-border">
+          <i class="material-icons orange-text dot-size ">fiber_manual_record</i>
+          <h5>
+            First
+          </h5>
+        </div>
+        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li>
+    </ul>
+
   </div>
 </div>
