ec8718408cb2270fe16be28bfca87443faeb4a0a
Diego-Melo <diego.nac@hotmail.com>
2021-01-21 15:07:40 -0300

    Adição das imagens do Núcleo Gestor

--- components/home/management.php
+++ components/home/management.php
@@ -1,74 +1,70 @@
 <div class="col l6 m12 s12 management">
   <h3 class="center atma teal-text text-darken-4">Núcleo Gestor</h3>
-  <ul class="collection">
-    <li class="collection-item avatar grey lighten-4">
-      <div class="row">
-        <div class="col s4">
-          <img src="assets/img/news/enem.png" alt="Diretora" class="circle">
+    <ul class="collection">
+      <li class="collection-item avatar grey lighten-4">
+        <div class="row">
+          <div class="col s4">
+            <img src="assets/img/management/margareth.jpg" alt="Diretora" class="circle">
+          </div>
+          <div class="col s4">
+            <h5>Margareth Calvalcante</h5>
+            <p>Diretora da Escola</p>
+          </div>
+          <div class="col s4">
+          </div>
         </div>
-        <div class="col s4">
-          <h5>Margareth Calvalcante</h5>
-          <p>Diretora da Escola</p>
+      </li>
+      <li class="collection-item avatar grey lighten-4">
+        <div class="row">
+          <div class="col s4">
+            <img src="assets/img/management/monique.jpg" alt="Coordenadora" class="circle">
+          </div>
+          <div class="col s4">
+            <h5>Margareth Calvalcante</h5>
+            <p>Diretora da Escola</p>
+          </div>
+          <div class="col s4">
+          </div>
         </div>
-        <div class="col s4">
+      </li>
+      <li class="collection-item avatar grey lighten-4">
+        <div class="row">
+          <div class="col s4">
+            <img src="assets/img/management/lucilene.jpg" alt="Coordenadora de Estágio" class="circle">
+          </div>
+          <div class="col s4">
+            <h5>Margareth Calvalcante</h5>
+            <p>Diretora da Escola</p>
+          </div>
+          <div class="col s4">
+          </div>
         </div>
-      </div>
-    </li>
-    <li class="collection-item avatar grey lighten-4">
-      <div class="row">
-        <div class="col s4">
-          <img src="assets/img/news/enem.png" alt="Diretora" class="circle">
+      </li>
+      <li class="collection-item avatar grey lighten-4">
+        <div class="row">
+          <div class="col s4">
+            <img src="assets/img/management/eduardo.jpg" alt="Coordenador" class="circle">
+          </div>
+          <div class="col s4">
+            <h5>Margareth Calvalcante</h5>
+            <p>Diretora da Escola</p>
+          </div>
+          <div class="col s4">
+          </div>
         </div>
-        <div class="col s4">
-          <h5>Margareth Calvalcante</h5>
-          <p>Diretora da Escola
-          </p>
+      </li>
+      <li class="collection-item avatar grey lighten-4">
+        <div class="row">
+          <div class="col s4">
+            <img src="assets/img/management/rosa.jpg" alt="Secretária" class="circle">
+          </div>
+          <div class="col s4">
+            <h5>Margareth Calvalcante</h5>
+            <p>Diretora da Escola</p>
+          </div>
+          <div class="col s4">
+          </div>
         </div>
-        <div class="col s4">
-        </div>
-      </div>
-    </li>
-    <li class="collection-item avatar grey lighten-4">
-      <div class="row">
-        <div class="col s4">
-          <img src="assets/img/news/enem.png" alt="Diretora" class="circle">
-        </div>
-        <div class="col s4">
-          <h5>Margareth Calvalcante</h5>
-          <p>Diretora da Escola
-          </p>
-        </div>
-        <div class="col s4">
-        </div>
-      </div>
-    </li>
-    <li class="collection-item avatar grey lighten-4">
-      <div class="row">
-        <div class="col s4">
-          <img src="assets/img/news/enem.png" alt="Diretora" class="circle">
-        </div>
-        <div class="col s4">
-          <h5>Margareth Calvalcante</h5>
-          <p>Diretora da Escola
-          </p>
-        </div>
-        <div class="col s4">
-        </div>
-      </div>
-    </li>
-    <li class="collection-item avatar grey lighten-4">
-      <div class="row">
-        <div class="col s4">
-          <img src="assets/img/news/enem.png" alt="Diretora" class="circle">
-        </div>
-        <div class="col s4">
-          <h5>Margareth Calvalcante</h5>
-          <p>Diretora da Escola
-          </p>
-        </div>
-        <div class="col s4">
-        </div>
-      </div>
-    </li>
-  </ul>
-</div>
+      </li>
+    </ul>
+  </div>
