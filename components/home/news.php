6a8aaa2afd477334a9b6b1cde2496c15f9cd0c18
JoseVitorNobre <josevitortinf@gmail.com>
2021-01-20 22:01:02 -0300

    Parte das Noticias adicionadas

--- components/home/news.php
+++ components/home/news.php
@@ -3,14 +3,72 @@
     <h2 class="atma white-text courses-heading news">
       Notícias
     </h2>
-    <div class="col s6 m4 l3">
+    <div class="col s3 m4 l3 pull-1">
       <div class="card news-card">
         <div class="card-image waves-effect waves-block waves-light">
           <img class="activator" src="assets/img/news/enem.png">
         </div>
         <div class="card-content">
           <span class="card-title activator grey-text text-darken-4">
-            ENEM
+            Lista de Aprovados 2021
+            <i class="material-icons right position-fixed">more_vert</i>
+          </span>
+        </div>
+        <div class="card-reveal">
+          <p>Aqui estão os alunos aprovados no processo seletivo da EEEP Joaquim Antônio Albano</p>
+          <p><a href="https://www.seduc.ce.gov.br/wp-content/uploads/sites/37/2021/01/final_joaquim_antonio_albano.pdf" target="_blank">Veja Aqui</a></p>
+          <div class="card-action center">
+            <span class="card-title grey-text text-darken-2 news-close">
+              Fechar
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col s3 m4 l3">
+      <div class="card news-card">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="/assets/img/news/matricula.webp" style="height: 15em;">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">
+            Matrículas
+            <i class="material-icons right position-fixed">more_vert</i>
+          </span>
+        </div>
+        <div class="card-reveal">
+          <p>
+            Horário: 8h às 12h/13h às 16h
+            Documentação Necessaria
+          <ul class="list-news">
+            <li>01 PASTA ESCOLAR;</li>
+            <li>02 FOTOS 3X4 IGUAIS E RECENTES;</li>
+            <li>XEROX DO REGISTRO DE NASCIMENTO, RG E CPF(DO ALUNO E DOS PAIS)PARA QUEM
+              NÃO ENTREGOU NO ATO DA INSCRIÇÃO;</li>
+            <li> HISTORICO ESCOLAR DO 1º AO 9º ANO OU DECLARAÇÃO DE CONCLUSÃO DO ENSINO
+              FUNDAMENTAL, ASSINADA E CARIMBADA PELO DIRETOR(A) E SECRETÁRIO(A);</li>
+            <li>XEROX DA CARTEIRA DE ESTUDANTE;</li>
+            <li>XEROX DO CARTÃO BOLSA FAMILIA PARA QUEM POSSUI(NIS DO ALUNO)</li>
+            <li>XEROX DO CARTÃO DE VACINA ATUALIZADO.</li>
+          </ul>
+          </p>
+          <p><a href="/assets/img/news/matriculaCompleta.webp" target="_blank">Veja o cartaz completo</a></p>
+          <div class="card-action center">
+            <span class="card-title grey-text text-darken-2 news-close">
+              Fechar
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col s3 m4 l3">
+      <div class="card news-card">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="assets/img/news/ENEM.webp" style="height: 15em;">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">
+            ENEM dias 17 e 24
             <i class="material-icons right position-fixed">more_vert</i>
           </span>
         </div>
@@ -25,139 +83,7 @@
         </div>
       </div>
     </div>
-    <div class="col s6 m4 l3">
-      <div class="card news-card">
-        <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="assets/img/news/enem.png">
-        </div>
-        <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4">
-            ENEM
-            <i class="material-icons right position-fixed">more_vert</i>
-          </span>
-        </div>
-        <div class="card-reveal">
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p><a href="#">Saiba mais!</a></p>
-          <div class="card-action center">
-            <span class="card-title grey-text text-darken-2 news-close">
-              Fechar
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col s6 m4 l3">
-      <div class="card news-card">
-        <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="assets/img/news/enem.png">
-        </div>
-        <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4">
-            ENEM
-            <i class="material-icons right position-fixed">more_vert</i>
-          </span>
-        </div>
-        <div class="card-reveal">
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p><a href="#">Saiba mais!</a></p>
-          <div class="card-action center">
-            <span class="card-title grey-text text-darken-2 news-close">
-              Fechar
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col s6 m4 l3">
-      <div class="card news-card">
-        <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="assets/img/news/enem.png">
-        </div>
-        <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4">
-            ENEM
-            <i class="material-icons right position-fixed">more_vert</i>
-          </span>
-        </div>
-        <div class="card-reveal">
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p><a href="#">Saiba mais!</a></p>
-          <div class="card-action center">
-            <span class="card-title grey-text text-darken-2 news-close">
-              Fechar
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col s6 m4 l3">
-      <div class="card news-card">
-        <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="assets/img/news/enem.png">
-        </div>
-        <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4">
-            ENEM
-            <i class="material-icons right position-fixed">more_vert</i>
-          </span>
-        </div>
-        <div class="card-reveal">
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p><a href="#">Saiba mais!</a></p>
-          <div class="card-action center">
-            <span class="card-title grey-text text-darken-2 news-close">
-              Fechar
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col s6 m4 l3">
-      <div class="card news-card">
-        <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="assets/img/news/enem.png">
-        </div>
-        <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4">
-            ENEM
-            <i class="material-icons right position-fixed">more_vert</i>
-          </span>
-        </div>
-        <div class="card-reveal">
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p><a href="#">Saiba mais!</a></p>
-          <div class="card-action center">
-            <span class="card-title grey-text text-darken-2 news-close">
-              Fechar
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col s6 m4 l3">
-      <div class="card news-card">
-        <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="assets/img/news/enem.png">
-        </div>
-        <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4">
-            ENEM
-            <i class="material-icons right position-fixed">more_vert</i>
-          </span>
-        </div>
-        <div class="card-reveal">
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p><a href="#">Saiba mais!</a></p>
-          <div class="card-action center">
-            <span class="card-title grey-text text-darken-2 news-close">
-              Fechar
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col s6 m4 l3">
+    <div class="col s3 m4 l3">
       <div class="card news-card">
         <div class="card-image waves-effect waves-block waves-light">
           <img class="activator" src="assets/img/news/enem.png">
