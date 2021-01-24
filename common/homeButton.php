b7b0c72526ec926d06dca79fd590a23163acc8db
JoseVitorNobre <josevitortinf@gmail.com>
2021-01-24 20:56:25 -0300

    Botao de voltar pro inicio na area inteira e footer reparado

--- common/homeButton.php
+++ common/homeButton.php
@@ -1,5 +1,5 @@
 <div class="fixed-action-btn">
-  <a class="btn-floating orange hide-on-large-only" id="return-to-top">
+  <a class="btn-floating orange" id="return-to-top">
     <i class="large material-icons">arrow_upward</i>
   </a>
 </div>
