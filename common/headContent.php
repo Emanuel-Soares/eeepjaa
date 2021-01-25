f0351f31655f1531a0c660dbf6358c3176fa9a84
Emanuel Soares <46508229+Emanuel-Soares@users.noreply.github.com>
2021-01-25 15:07:59 -0300

    Update headContent.php

--- common/headContent.php
+++ common/headContent.php
@@ -14,9 +14,7 @@
 <link href="https:/fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <script src="https:/code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https:/cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-<link rel="stylesheet" href="/assets/css/home/materialize/css/materialize.min.css">
 <script src="https:/cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-<link rel="stylesheet" href="/assets/css/home/materialize/js/materialize.min.js">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Atma:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
