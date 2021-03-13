a87d2529d68dd04cc32c384c8665d9db18f46265
Emanuel-Soares <emanuelsevero869@gmail.com>
2021-03-13 15:03:54 -0300

    fix: numbers component responsive

--- components/home/numbers.php
+++ components/home/numbers.php
@@ -6,28 +6,34 @@
       </h2>
     </div>
     <div class="row numbers-content">
-      <div class="col s12 m12 l4">
-        <div class="number-circle">
-          <img src="/assets/icons/gestao.webp" alt="Ingressantes">
-          <span class="center white-text number">120</span>
-          <span class="center white-text number-title">Ingressante</span>
+      <div class="col s12 m6 l4">
+        <div class="number-spacing">
+          <div class="number-circle">
+            <img src="/assets/icons/gestao.webp" alt="Ingressantes">
+            <span class="center white-text number">120</span>
+            <span class="center white-text number-title">Ingressantes</span>
+          </div>
         </div>
       </div>
-      <div class="col s12 m12 l4">
-        <div class="number-circle">
-          <img src="/assets/icons/integrantes.webp" alt="Matriculados">
-          <span class="center white-text number">378</span>
-          <span class="center white-text number-title">Matriculados em 2020</span>
+      <div class="col s12 m6 l4">
+        <div class="number-spacing">
+          <div class="number-circle">
+            <img src="/assets/icons/integrantes.webp" alt="Matriculados">
+            <span class="center white-text number">378</span>
+            <span class="center white-text number-title">Matriculados em 2020</span>
+          </div>
         </div>
       </div>
-      <div class="col s12 m12 l4">
-        <div class="number-circle">
-          <img src="/assets/icons/aprovados.webp" alt="Aprovados">
-          <span class="center white-text number">21</span>
-          <span class="center white-text number-title">Aprovados UECE 2020</span>
+      <div class="col s12 m6 l4 push-m3">
+        <div class="number-spacing">
+          <div class="number-circle">
+            <img src="/assets/icons/aprovados.webp" alt="Aprovados">
+            <span class="center white-text number">21</span>
+            <span class="center white-text">Aprovados UECE 2020</span>
+          </div>
         </div>
       </div>
-      <!-- <div class="col s12 m12 l4">
+      <!-- <div class="col s12 m6 l4">
         <div class="number-circle">
           <img src="/assets/icons/integrantes.webp" alt="">
           <span class="center white-text number">60</span>
