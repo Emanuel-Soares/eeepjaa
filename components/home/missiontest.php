3604b67e3e73fd1e2a62c7749ce01563dd792cca
Emanuel Soares <46508229+Emanuel-Soares@users.noreply.github.com>
2021-03-18 21:22:37 -0300

    Update missiontest.php

--- components/home/missiontest.php
+++ components/home/missiontest.php
@@ -11,6 +11,10 @@
     padding: 0;
     margin: 0;
   }
+  
+  .text-bold {
+    font-weight: 800; 
+  }
 
   .mission-test div {
     width: 50%;
@@ -36,6 +40,8 @@
 <div class="background-text mission-test">
   <div class="teal-text text-darken-4 center">
     <h3 style="margin: 0;" class="center atma">Missão</h3>
-    Oferecer educação de qualidade, visando tanto a entrada no mercado de trabalho quanto na Universidade. Dentre as melhores Escolas Públicas cearenses, a EEEP Joaquim Albano conta com 56 anos de tradição, atendendo aos mais diversos públicos da capital. Situada à rua Julio Siqueira, 390, a escola conta com 1000m² de área, com rampas de acessibilidade, quadra poliesportiva, laboratórios de ciências e informática e professores qualificados, comprometidos com o processo de ensino e aprendizagem
+    <p class="text-bold">
+      Oferecer educação de qualidade, visando tanto a entrada no mercado de trabalho quanto na Universidade. Dentre as melhores Escolas Públicas cearenses, a EEEP Joaquim Albano conta com 56 anos de tradição, atendendo aos mais diversos públicos da capital. Situada à rua Julio Siqueira, 390, a escola conta com 1000m² de área, com rampas de acessibilidade, quadra poliesportiva, laboratórios de ciências e informática e professores qualificados, comprometidos com o processo de ensino e aprendizagem.
+    </p>
   </div>
 </div>
