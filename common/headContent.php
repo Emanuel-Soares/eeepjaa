835f43cb27d89a4219c05fec13781b8dcde01e22
Emanuel-Soares <emanuelsevero869@gmail.com>
2021-03-19 11:12:00 -0300

    refactor(common): link to materialize and jquery to local

--- common/headContent.php
+++ common/headContent.php
@@ -11,10 +11,10 @@
 <link rel="manifest" href="/assets/icons/favicon/site.webmanifest">
 
 <!-- CDN -->
+<script src="/assets/js/materialize/jquery.min.js"></script>
 <link href="https:/fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<script src="https:/code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https:/cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-<script src="https:/cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+<link rel="stylesheet" href="/assets/css/materialize/materialize.min.css">
+<script src="/assets/js/materialize/materialize.min.js"></script>
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Atma:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
