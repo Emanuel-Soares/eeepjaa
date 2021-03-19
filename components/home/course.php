84cdfde5f494b55861ac6760200e51769fb55703
Diego-Melo <diego.nac@hotmail.com>
2021-03-19 00:03:52 -0300

    Adição do card Saúde Bucal

--- components/home/course.php
+++ components/home/course.php
@@ -2,123 +2,120 @@
   <h2 class="atma white-text courses-heading">
     Cursos Técnicos
   </h2>
-  <center>
-    <div class="col l3 m6 s12">
-      <div class="card align-card-content">
-        <div class="card-image">
-          <img class="activator" src="/assets/img/home/informatica.webp" alt="Informática" />
-        </div>
-        <div class="card-content content-spacing card-title grey-text text-darken-4 text-normal left-align align-span">
-          <span class="activator">Informática</span> <br /> &nbsp;
-          <i class="material-icons right activator">more_vert</i>
-        </div>
-        <div class="card-reveal">
-          <div class="content-reveal">
-            <span class="card-title grey-text text-darken-4">Informática</span>
-            <p>
-              O Curso de informática consiste em inserir alunos no mercado de trabalho, através de uma
-              grade que possibilite conhecimentos em informática básica, gestão do tempo,
-              noções de robótica, entre outros.
-              <br>
-              <a href="/cursos#informatica">Saiba mais</a>
-            </p>
-          </div>
-          <div class="card-action">
-            <span class="card-title teal-text text-darken-2 right">
-              Fechar
-            </span>
-          </div>
+  <div class="col l3 m6 s12">
+    <div class="card align-card-content">
+      <div class="card-image">
+        <img class="activator" src="/assets/img/home/informatica.webp" alt="Informática" />
+      </div>
+      <div class="card-content content-spacing card-title grey-text text-darken-4 text-normal left-align align-span">
+        <span class="activator">Informática</span> <br /> &nbsp;
+        <i class="material-icons right activator">more_vert</i>
+      </div>
+      <div class="card-reveal">
+        <div class="content-reveal">
+          <span class="card-title grey-text text-darken-4">Informática</span>
+          <p>
+            O Curso de informática consiste em inserir alunos no mercado de trabalho, através de uma
+            grade que possibilite conhecimentos em informática básica, gestão do tempo,
+            noções de robótica, entre outros.
+            <br>
+            <a href="/cursos#informatica">Saiba mais</a>
           </p>
         </div>
+        <div class="card-action">
+          <span class="card-title teal-text text-darken-2 right">
+            Fechar
+          </span>
+        </div>
+        </p>
       </div>
     </div>
-    <div class="col l3 m6 s12">
-      <div class="card align-card-content">
-        <div class="card-image">
-          <img class="activator" src="/assets/img/home/enfermagem.webp" alt="Enfermagem" class="image-responsive">
-        </div>
-        <div class="card-content content-spacing card-title grey-text text-darken-4 text-normal left-align align-span">
-          <span class="activator">Enfermagem</span> <br /> &nbsp;
-          <i class="material-icons right activator">more_vert</i>
-        </div>
-        <div class="card-reveal">
-          <div class="content-reveal">
-            <span class="card-title grey-text text-darken-4">
-              Enfermagem
-            </span>
-            <p>
-              O curso prepara o aluno para atuar de forma ética e humanizada no cuidado dos pacientes e promoção da saúde individual e coletiva. Nos anos iniciais do curso, são aplicadas disciplinas teóricas, como Anatomia, Fisiologia e Imunologia.
-              <br>
-              <a href="/cursos#enfermagem">Saiba mais</a>
-            </p>
-          </div>
-          <div class="card-action">
-            <span class="card-title teal-text text-darken-2 right">
-              Fechar
-            </span>
-          </div>
-        </div>
+  </div>
+  <div class="col l3 m6 s12">
+    <div class="card align-card-content">
+      <div class="card-image">
+        <img class="activator" src="/assets/img/home/enfermagem.webp" alt="Enfermagem" class="image-responsive">
       </div>
-    </div>
-
-
-    <div class="col l3 m8 s12 push-m2">
-      <div class="card align-card-content">
-        <div class="card-image">
-          <img class="activator" src="/assets/img/home/seguranca.webp" alt="Segurança do Trabalho">
+      <div class="card-content content-spacing card-title grey-text text-darken-4 text-normal left-align align-span">
+        <span class="activator">Enfermagem</span> <br /> &nbsp;
+        <i class="material-icons right activator">more_vert</i>
+      </div>
+      <div class="card-reveal">
+        <div class="content-reveal">
+          <span class="card-title grey-text text-darken-4">
+            Enfermagem
+          </span>
+          <p>
+            O curso prepara o aluno para atuar de forma ética e humanizada no cuidado dos pacientes e promoção da saúde individual e coletiva. Nos anos iniciais do curso, são aplicadas disciplinas teóricas, como Anatomia, Fisiologia e Imunologia.
+            <br>
+            <a href="/cursos#enfermagem">Saiba mais</a>
+          </p>
         </div>
-        <div class="card-content content-spacing card-title activator grey-text text-darken-4 text-normal left-align align-span">
-          <span class="activator">Segurança do Trabalho</span> <br /> &nbsp;
-          <i class="material-icons right activator">more_vert</i>
-        </div>
-        <div class="card-reveal">
-          <div class="content-reveal">
-            <span class="card-title grey-text text-darken-4">
-              Segurança do Trabalho
-            </span>
-            <p>
-              Neste curso você vai estudar ações preventivas para garantia da saúde e segurança de trabalhadores. Vai aprender técnicas de identificação, avaliação e controle de riscos, analisar acidentes e recomendar medidas de prevenção.
-              <br>
-              <a href="/cursos#segurança">Saiba mais</a>
-            </p>
-          </div>
-          <div class="card-action">
-            <span class="card-title teal-text text-darken-2 right">
-              Fechar
-            </span>
-          </div>
+        <div class="card-action">
+          <span class="card-title teal-text text-darken-2 right">
+            Fechar
+          </span>
         </div>
       </div>
     </div>
+  </div>
 
-    <div class="col l3 m8 s12 push-m2">
-      <div class="card align-card-content">
-        <div class="card-image">
-          <img class="activator" src="/assets/img/home/bucal.webp" alt="Saúde bucal">
+
+  <div class="col l3 m8 s12 push-m2">
+    <div class="card align-card-content">
+      <div class="card-image">
+        <img class="activator" src="/assets/img/home/seguranca.webp" alt="Segurança do Trabalho">
+      </div>
+      <div class="card-content content-spacing card-title activator grey-text text-darken-4 text-normal left-align align-span">
+        <span class="activator">Segurança do Trabalho</span> <br /> &nbsp;
+        <i class="material-icons right activator">more_vert</i>
+      </div>
+      <div class="card-reveal">
+        <div class="content-reveal">
+          <span class="card-title grey-text text-darken-4">
+            Segurança do Trabalho
+          </span>
+          <p>
+            Neste curso você vai estudar ações preventivas para garantia da saúde e segurança de trabalhadores. Vai aprender técnicas de identificação, avaliação e controle de riscos, analisar acidentes e recomendar medidas de prevenção.
+            <br>
+            <a href="/cursos#segurança">Saiba mais</a>
+          </p>
         </div>
-        <div class="card-content content-spacing card-title activator grey-text text-darken-4 text-normal left-align align-span">
-          <span class="activator">Saúde bucal</span> <br /> &nbsp;
-          <i class="material-icons right activator">more_vert</i>
-        </div>
-        <div class="card-reveal">
-          <div class="content-reveal">
-            <span class="card-title grey-text text-darken-4">
-              Saúde bucal
-            </span>
-            <p>
-            Neste curso você é preparado para prestar auxílio ao cirurgião-dentista durante o atendimento. Além de cuidados como promoções, prevenções e controle de doenças bucais.
-              <br>
-              <a href="/cursos#bucal">Saiba mais</a>
-            </p>
-          </div>
-          <div class="card-action">
-            <span class="card-title teal-text text-darken-2 right">
-              Fechar
-            </span>
-          </div>
+        <div class="card-action">
+          <span class="card-title teal-text text-darken-2 right">
+            Fechar
+          </span>
         </div>
       </div>
     </div>
+  </div>
 
-  </center>
+  <div class="col l3 m8 s12 push-m2">
+    <div class="card align-card-content">
+      <div class="card-image">
+        <img class="activator" src="/assets/img/home/bucal.webp" alt="Saúde bucal">
+      </div>
+      <div class="card-content content-spacing card-title activator grey-text text-darken-4 text-normal left-align align-span">
+        <span class="activator">Saúde bucal</span> <br /> &nbsp;
+        <i class="material-icons right activator">more_vert</i>
+      </div>
+      <div class="card-reveal">
+        <div class="content-reveal">
+          <span class="card-title grey-text text-darken-4">
+            Saúde bucal
+          </span>
+          <p>
+            Neste curso você é preparado para prestar auxílio ao cirurgião-dentista durante o atendimento. Além de cuidados como promoções, prevenções e controle de doenças bucais. O técnico em saúde bucal estuda algumas disciplinas como Radiologia Bucal, Técnicas Restauradoras e Saúde Ambiental.
+            <br>
+            <a href="/cursos#bucal">Saiba mais</a>
+          </p>
+        </div>
+        <div class="card-action">
+          <span class="card-title teal-text text-darken-2 right">
+            Fechar
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
